6322402a9dbb094fe487ee684de5037867f2c2dc
ghassen3699 <ghassenkhammessi123@icloud.com>
2020-08-10 11:00:30 +0100

    hackerearth exercises

---
+++ sum_of_four_integers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main(void) {
+	
+	int numArray[4];   // Define an array of four integers
+        int i ;
+	// Get inputs for the array elements
+	for (i=0; i<4; i++)
+	{
+    	scanf("%d", &numArray[i]);
+	}
+    
+	int sum = 0;
+	// Write the logic to add these integers here:
+	for (i = 0 ; i < 4 ; i++)
+	{
+		sum = sum +numArray[i] ;
+	}
+	
+ 
+    
+	
+	printf("%d\n",sum);   // Print the sum
+    
+	return 0;
+}
